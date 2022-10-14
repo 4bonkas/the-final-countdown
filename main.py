539e3f160be1e175d07637725b267a413eac9885
4bonkas <109120381+4bonkas@users.noreply.github.com>
2022-10-14 12:23:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import requests
 import webbrowser
 # grab plaintext from endopoints
-e1 = requests.get('https://roambarcelona.com/clock-pt1?verify=Na2Q%2BeqhSP5hTRLDwpTNoA%3D%3D')
-e2 = requests.get('https://roambarcelona.com/clock-pt2?verify=Na2Q%2BeqhSP5hTRLDwpTNoA%3D%3D')
-e3 = requests.get('https://roambarcelona.com/clock-pt3?verify=Na2Q%2BeqhSP5hTRLDwpTNoA%3D%3D')
-e4 = requests.get('https://roambarcelona.com/clock-pt4?verify=Na2Q%2BeqhSP5hTRLDwpTNoA%3D%3D')
-e5 = requests.get('https://roambarcelona.com/clock-pt5?verify=Na2Q%2BeqhSP5hTRLDwpTNoA%3D%3D')
+slay1 = requests.get('https://roambarcelona.com/clock-pt1?verify=Na2Q%2BeqhSP5hTRLDwpTNoA%3D%3D') # replace each one of these with your link
+slay2 = requests.get('https://roambarcelona.com/clock-pt2?verify=Na2Q%2BeqhSP5hTRLDwpTNoA%3D%3D') # replace each one of these with your link
+slay3 = requests.get('https://roambarcelona.com/clock-pt3?verify=Na2Q%2BeqhSP5hTRLDwpTNoA%3D%3D') # replace each one of these with your link
+slay4 = requests.get('https://roambarcelona.com/clock-pt4?verify=Na2Q%2BeqhSP5hTRLDwpTNoA%3D%3D') # replace each one of these with your link
+slay5 = requests.get('https://roambarcelona.com/clock-pt5?verify=Na2Q%2BeqhSP5hTRLDwpTNoA%3D%3D') # replace each one of these with your link
 
-#Collect text information from endpoints
-secretKey = (e1.text + e2.text + e3.text + e4.text + e5.text)
+# collect text from endpoints
+secretLink = (slay1.text + slay2.text + slay3.text + slay4.text + slay5.text)
 
-#Open a url in browser
-final = ('https://roambarcelona.com/get-flag?verify=Na2Q%2BeqhSP5hTRLDwpTNoA%3D%3D&string='+secretKey)
-webbrowser.open_new_tab(final)
+# open secret link in browser
+bestSlay = ('https://roambarcelona.com/get-flag?verify=Na2Q%2BeqhSP5hTRLDwpTNoA%3D%3D&string='+secretLink)
+webbrowser.open_new_tab(bestSlay)
